120067c02eaaab71493aa78fb45e38b357c73086
Shubham Avasthi <shubhamavasthi1@gmail.com>
2017-06-01 03:42:12 +0530

    Update models.py

--- spardha/rest_serv/models.py
+++ spardha/rest_serv/models.py
@@ -1,30 +1,44 @@
 from django.db import models
 
+class College(models.Model):
+    CollegeID=models.AutoField(primary_key=True,editable=False)
+    CollegeName=models.CharField(max_length=40,unique=True,editable=False)
+    Players=models.ManyToManyField(Player,on_delete=models.CASCADE,blank=True,null=True)
+
 class Player(models.Model):
-    PlayerID=models.IntegerField(primary_key=True)
+    PlayerID=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     PlayerName=models.CharField(max_length=40)
     PlayerImage=models.ImageField(blank=True,null=True)
-    CollegeID=models.IntegerField()
+    CollegeID=models.ForeignKey(College,on_delete=models.CASCADE,blank=True,null=True)
     Email=models.EmailField()
-    PhoneNumber=models.CharField(max_length=10,blank=True,null=True)
+    PhoneNumber=models.CharField(max_length=15,blank=True,null=True)
 
 class Team(models.Model):
-    TeamID=models.IntegerField(primary_key=True)
-    CoachName=models.CharField(max_length=40)
-    CollegeID=models.IntegerField()
-    LeaderID=models.IntegerField(blank=True,null=True)
+    TeamID=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    CoachName=models.CharField(max_length=40,blank=True,null=True)
+    CollegeID=models.ForeignKey(College,on_delete=models.CASCADE,blank=True,null=True)
+    Leader=models.ForeignKey(Player, on_delete=models.CASCADE,blank=True,null=True)
+    Members=models.ManyToManyField(Player,on_delete=models.CASCADE)
+    Event=models.ForeignKey(Event,on_delete=models.CASCADE)
 
 class Event(models.Model):
-    EventID=models.IntegerField(primary_key=True)
+    EventID=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     EventName=models.CharField(max_length=25)
     Venue=models.TextField(max_length=300,blank=True,null=True)
     EventDateTime=models.DateTimeField(blank=True,null=True)
-    MinPlayersReq=models.IntegerField(default=1)
-    MaxPlayersReq=models.IntegerField(default=100)
+    MinPlayersReq=models.IntegerField(default=0)
+    MaxPlayersReq=models.IntegerField(default=0)
+    RegisteredTeams=models.ManyToManyField(Team,on_delete=models.CASCADE,blank=True,null=True)
+    RegisteredPlayers=models.ManyToManyField(Player,on_delete=models.CASCADE,blank=True,null=True)
 
-class Notification(models.Model)
-    NotificationID=models.IntegerField(primary_key=True)
+class PersistentNotification(models.Model)
+    NotificationID=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     NotificationText=models.CharField(max_length=200)
     Priority=models.IntegerField(default=1,blank=True)
-    IsPersistent=BooleanField(default=False,blank=True)
-    AimedEventID=models.IntegerField(blank=True,null=True)
+    
+class UserNotification(models.Model)
+    NotificationID=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    NotificationText=models.CharField(max_length=200)
+    Priority=models.IntegerField(default=1,blank=True)
+    UserToNotify=models.ForeignKey(Player,on_delete=models.CASCADE)
+    
