5dabda471efb031aefa3153ed3fb5d578eac71ea
vaishnavsm <vaishnavsm@gmail.com>
2017-06-01 12:05:55 +0530

    Add Utility Functions to Models Added fuctions to create notifications

--- spardha/rest_serv/models.py
+++ spardha/rest_serv/models.py
@@ -12,6 +12,13 @@ class Player(models.Model):
     CollegeID=models.ForeignKey(College,on_delete=models.CASCADE,blank=True,null=True)
     Email=models.EmailField()
     PhoneNumber=models.CharField(max_length=15,blank=True,null=True)
+    def Notify(self, Message, Priority=1):
+        notification = UserNotification(NotificationText=Message, Priority=Priority, UserToNotify=self)
+        notification.save()
+        return
+    def GetNotifications(self):
+        notifications = UserNotification.objects.filter(UserToNotify=self)
+        return notifications
 
 class Team(models.Model):
     TeamID=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -20,7 +27,11 @@ class Team(models.Model):
     Leader=models.ForeignKey(Player, on_delete=models.CASCADE,blank=True,null=True)
     Members=models.ManyToManyField(Player,on_delete=models.CASCADE)
     Event=models.ForeignKey(Event,on_delete=models.CASCADE)
-
+    def NotifyTeam(self, Message, Priority=1):
+        for player in self.Members.all():
+            player.Notify(Message, Priority)
+        return
+    
 class Event(models.Model):
     EventID=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     EventName=models.CharField(max_length=25)
@@ -29,8 +40,11 @@ class Event(models.Model):
     MinPlayersReq=models.IntegerField(default=0)
     MaxPlayersReq=models.IntegerField(default=0)
     RegisteredTeams=models.ManyToManyField(Team,on_delete=models.CASCADE,blank=True,null=True)
-    RegisteredPlayers=models.ManyToManyField(Player,on_delete=models.CASCADE,blank=True,null=True)
-
+    def NotifyTeams(self, Message, Priority=1):
+        for team in self.RegisteredTeams.all():
+            team.NotifyTeam(Message, Priority)
+        return
+    
 class PersistentNotification(models.Model):
     NotificationID=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     NotificationText=models.CharField(max_length=200)
